fe4c3925f4410eaefb7d13fe2bbc13820a9a4643
Shruti <agarwal.shruti1302@gmail.com>
2020-11-10 23:24:37 +0530

    Update Satellite Page

--- src/components/Satellite.js
+++ src/components/Satellite.js
@@ -60,59 +60,6 @@ impact on agriculture, forest, geology and the environment is crucial in order
 to improve the population’s living conditions. These satellites can be used for capturing images of the Earth, Earth Mapping and Astronomy.'
         image="url('https://www.nasa.gov/sites/default/files/iss038e046586_0.jpg')"
       />
-//       <TypeCard
-//         title='Environmental Satellites'
-//         content='Nulla aliquet porttitor lacus luctus accumsan. Nibh ipsum
-//                     consequat nisl vel pretium lectus. Nec dui nunc mattis enim
-//                     ut tellus elementum sagittis vitae. Ullamcorper a lacus
-//                     vestibulum sed arcu non odio. Eget nunc lobortis mattis
-//                     aliquam faucibus. Risus nullam eget felis eget nunc lobortis
-//                     mattis aliquam faucibus. Morbi tempus iaculis urna id. Leo a
-//                     diam sollicitudin tempor id. Aliquam purus sit amet luctus.
-//                     Auctor augue mauris augue neque gravida in fermentum et
-//                     sollicitudin. Nulla facilisi morbi tempus iaculis urna id
-//                     volutpat. Tincidunt eget nullam non nisi est sit amet.'
-//         image="url('https://www.cgi-group.co.uk/sites/default/files/styles/hero_banner/public/italy_0.jpg?itok=1PYz25_A')"
-//       />
-      {/* <TypeCard
-        title='Earth Observing Satellites'
-        content='Nulla aliquet porttitor lacus luctus accumsan. Nibh ipsum
-                    consequat nisl vel pretium lectus. Nec dui nunc mattis enim
-                    ut tellus elementum sagittis vitae. Ullamcorper a lacus
-                    vestibulum sed arcu non odio. Eget nunc lobortis mattis
-                    aliquam faucibus. Risus nullam eget felis eget nunc lobortis
-                    mattis aliquam faucibus. Morbi tempus iaculis urna id. Leo a
-                    diam sollicitudin tempor id. Aliquam purus sit amet luctus.
-                    Auctor augue mauris augue neque gravida in fermentum et
-                    sollicitudin. Nulla facilisi morbi tempus iaculis urna id
-                    volutpat. Tincidunt eget nullam non nisi est sit amet.'
-      />
-      <TypeCard
-        title='Weather Satellites'
-        content='Nulla aliquet porttitor lacus luctus accumsan. Nibh ipsum
-                    consequat nisl vel pretium lectus. Nec dui nunc mattis enim
-                    ut tellus elementum sagittis vitae. Ullamcorper a lacus
-                    vestibulum sed arcu non odio. Eget nunc lobortis mattis
-                    aliquam faucibus. Risus nullam eget felis eget nunc lobortis
-                    mattis aliquam faucibus. Morbi tempus iaculis urna id. Leo a
-                    diam sollicitudin tempor id. Aliquam purus sit amet luctus.
-                    Auctor augue mauris augue neque gravida in fermentum et
-                    sollicitudin. Nulla facilisi morbi tempus iaculis urna id
-                    volutpat. Tincidunt eget nullam non nisi est sit amet.'
-      />
-      <TypeCard
-        title='Defence Satellites'
-        content='Nulla aliquet porttitor lacus luctus accumsan. Nibh ipsum
-                    consequat nisl vel pretium lectus. Nec dui nunc mattis enim
-                    ut tellus elementum sagittis vitae. Ullamcorper a lacus
-                    vestibulum sed arcu non odio. Eget nunc lobortis mattis
-                    aliquam faucibus. Risus nullam eget felis eget nunc lobortis
-                    mattis aliquam faucibus. Morbi tempus iaculis urna id. Leo a
-                    diam sollicitudin tempor id. Aliquam purus sit amet luctus.
-                    Auctor augue mauris augue neque gravida in fermentum et
-                    sollicitudin. Nulla facilisi morbi tempus iaculis urna id
-                    volutpat. Tincidunt eget nullam non nisi est sit amet.'
-      /> */}
     </>
   );
 };
