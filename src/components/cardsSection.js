514ed921af5f03004703dd04305d8f09ccdd9c10
Rajat Baxi <51674643+Rajatino16@users.noreply.github.com>
2020-11-10 19:50:37 +0530

    flash cards as activities in cardSection.js

--- src/components/cardsSection.js
+++ src/components/cardsSection.js
@@ -7,34 +7,35 @@ const cardsSection = () => {
       <div className='container'>
         <div className='row'>
           <div className='col-10 content-container'>
-            <div className='section-heading mb-5'>Section Title</div>
+            <div className='section-heading mb-5'>Activities</div>
 
             <div className='row'>
               <div className='col-4'>
                 <Card
                   cardImage='https://www.esa.int/var/esa/storage/images/19716864-11-eng-GB/ESA_root_pillars.jpg'
-                  cardTitle='Card Title1'
-                  cardContent='This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.'
+                  cardTitle='We intend to develop a CubeSat satellite'
+                  cardContent='
+A CubeSat (U-class spacecraft) is a type of miniaturized satellite for space research that is made up of
+multiples of 10 cm × 10 cm × 10 cm cubic unit.CubeSats have a mass of no more than 1.33 kilograms (2.9
+lb) per unit and often use commercial off-the-shelf (COTS) components for their electronics and structure. '
                 />
               </div>
               <div className='col-4'>
                 <Card
                   cardImage='https://www.esa.int/var/esa/storage/images/19716864-11-eng-GB/ESA_root_pillars.jpg'
-                  cardTitle='Card Title1'
-                  cardContent='This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.'
+                  cardTitle='Training Sessions'
+                  cardContent='Regular online sessions by university faculties to gain expertise in design and development of the
+satellite. Conducting training sessions for designing the 3D models of satellites. Events and Seminar for giving awareness about latest trends in space technology.'
                 />
               </div>
               <div className='col-4'>
                 <Card
                   cardImage='https://www.esa.int/var/esa/storage/images/19716864-11-eng-GB/ESA_root_pillars.jpg'
-                  cardTitle='Card Title1'
-                  cardContent='This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.'
+                  cardTitle='Student\'s Profile'
+                  cardContent='Satellite team provides a unique, hands-on opportunity for students from a different semesters in the
+college to collaborate in a meaningful way for the purpose of advancing space technology. Most importantly,students will actually be launching technologically advanced satellites into space.
+This club will set a new bar for student-built technology by demonstrating an actual satellite built and
+launched by students during their college years itself. '
                 />
               </div>
             </div>
