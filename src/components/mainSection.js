65f1ec8b9577b9f952abfd34f598fa307b0ef7ef
Rajat Baxi <51674643+Rajatino16@users.noreply.github.com>
2020-11-10 12:33:39 +0530

    Motto in mainSection.js

--- src/components/mainSection.js
+++ src/components/mainSection.js
@@ -18,8 +18,7 @@ const mainSection = () => {
                 {/* <span className='muj'>Manipal University Jaipur</span> */}
               </div>
               <div className='tagline'>
-                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-                eiusmod!
+                ##### MOTTO #####
               </div>
             </div>
           </div>
