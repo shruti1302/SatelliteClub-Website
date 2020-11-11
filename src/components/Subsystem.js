cd2c802ff614d26c9ff6c77789bcfee32f01e150
Rajat Baxi <51674643+Rajatino16@users.noreply.github.com>
2020-11-11 15:48:38 +0530

    corrected html issue

--- src/components/Subsystem.js
+++ src/components/Subsystem.js
@@ -31,8 +31,8 @@ const Subsystems = () => {
       />
       <ContentSection
         title='Subsystem'
-        content='In satellite communication system, various operations take place. Among which, the main operations are orbit controlling, altitude of satellite, monitoring and controlling of other subsystems. <br>
-        A satellite communication consists of mainly two segments. Those are space segment and earth segment. So, accordingly there will be two types of subsystems namely, space segment subsystems and earth segment subsystems. The subsystems present in space segment are called as space segment subsystems. Following are the space segment subsystems: AOC Subsystem, TTCM Subsystem, Power and Antenna Subsystems, Transponders.<br>
+        content='In satellite communication system, various operations take place. Among which, the main operations are orbit controlling, altitude of satellite, monitoring and controlling of other subsystems.
+        A satellite communication consists of mainly two segments. Those are space segment and earth segment. So, accordingly there will be two types of subsystems namely, space segment subsystems and earth segment subsystems. The subsystems present in space segment are called as space segment subsystems. Following are the space segment subsystems: AOC Subsystem, TTCM Subsystem, Power and Antenna Subsystems, Transponders.
         The subsystems present in the ground segment have the ability to access the satellite repeater in order to provide the communication between the users. Earth segment is also called as ground segment. Earth segment performs mainly two functions. Those are transmission of a signal to the satellite and reception of signal from the satellite. Earth stations are the major subsystems that are present in earth segment.
         '
       />
@@ -44,7 +44,7 @@ We can make this AOC subsystem into the following two parts: Altitude Control Su
       />
       <TypeCard
         title='TTCM Subsystem'
-        content='Telemetry, Tracking, Commanding and Monitoring (TTCM) subsystem is present in both satellite and earth station. In general, satellite gets data through sensors. So, Telemetry subsystem present in the satellite sends this data to earth station(s). Therefore, TTCM subsystem is very much necessary for any communication satellite in order to operate it successfully.<br>
+        content='Telemetry, Tracking, Commanding and Monitoring (TTCM) subsystem is present in both satellite and earth station. In general, satellite gets data through sensors. So, Telemetry subsystem present in the satellite sends this data to earth station(s). Therefore, TTCM subsystem is very much necessary for any communication satellite in order to operate it successfully.
 It is the responsibility of satellite operator in order to control the satellite in its life time, after placing it in the proper orbit. This can be done with the help of TTCM subsystem.'
         image="url('https://images.tmcnet.com/tmc/misc/articles/image/2018-apr/1371908675-bigstock--satellite-supersize.jpg')"
       />
@@ -52,10 +52,9 @@ It is the responsibility of satellite operator in order to control the satellite
         title='Power and Antenna Subsystems'
         content='We know that the satellite present in an orbit should be operated continuously during its life span. So, the satellite requires internal power in order to operate various electronic systems and communications payload that are present in it.
 Power system is a vital subsystem, which provides the power required for working of a satellite. Mainly, the solar cells (or panels) and rechargeable batteries are used in these systems.
-  <br>
-    Antennas are present in both satellite and earth station. Now, let us discuss about the satellite antennas.
+    Antennas are present in both satellite and earth station. 
 Satellite antennas perform two types of functions. Those are receiving of signals, which are coming from earth station and transmitting signals to one or more earth stations based on the requirement. In other words, the satellite antennas receive uplink signals and transmit downlink signals.
-We know that the length of satellite antennas is inversely proportional to the operating frequency. The operating frequency has to be increased in order to reduce the length of satellite antennas. Therefore, satellite antennas operate in the order of GHz frequencies.'
+'
       />
       <TypeCard
         title='Transponders'
